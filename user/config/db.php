bf70949fc88293e3de59d605b480c5711e4c428b
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-08-25 12:56:12 +0530

    db setup

--- user/config/db.php
+++ user/config/db.php
@@ -1,7 +1,13 @@
 <?php
     
+    $dbhost = "127.0.0.1:3306";
+    $dbuser = "u706182210_root";
+    $dbpass = "easylearn@1";
+    $dbname = "u706182210_easyearn";
+
     // $con = mysqli_connect("localhost","root","","easyearn");
-    $con = mysqli_connect("127.0.0.1:3306","u706182210_root","easylearn@1","u706182210_easyearn");
+    
+    $con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
