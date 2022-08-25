47c29722a6e698a5668a5415f8ee27b0c1b5d643
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-08-25 12:59:15 +0530

    db setup #2

--- user/config/db.php
+++ user/config/db.php
@@ -1,16 +1,16 @@
 <?php
     
-    $dbhost = "127.0.0.1:3306";
-    $dbuser = "u706182210_root";
-    $dbpass = "easylearn@1";
-    $dbname = "u706182210_easyearn";
+    $hostname = "127.0.0.1:3306";
+    $username = "u706182210_root";
+    $password = "easylearn@1";
+    $database = "u706182210_easyearn";
 
     // $con = mysqli_connect("localhost","root","","easyearn");
     
-    $con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+    // $con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    // if (mysqli_connect_errno()){
+    //     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    // }
 
-
+    $con = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
 ?>
